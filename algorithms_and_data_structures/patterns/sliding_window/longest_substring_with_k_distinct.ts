01b47c41b6182c91f23fbef9abaf55775dba428d
kadetman <kadetman83@gmail.com>
2021-12-08 13:24:13 -0800

    sliding window - longest substring with k distinct

---
+++ algorithms_and_data_structures/patterns/sliding_window/longest_substring_with_k_distinct.ts
@@ -0,0 +1,31 @@
+import { log } from '../../linked_list/utils';
+
+// https://www.educative.io/courses/grokking-the-coding-interview/YQQwQMWLx80
+function longestSubstringWithKDistinct(str: string, k: number): number {
+  let countByChar: Map<string, number> = new Map();
+  let maxLength = 0;
+
+  for (let start = 0, end = 0; end < str.length; end++) {
+    const char = str[end];
+    const count = countByChar.get(char) || 0;
+    countByChar.set(char, count + 1);
+
+    if (countByChar.size > k) {
+      maxLength = Math.max(maxLength, end - start);
+
+      while (countByChar.size > k) {
+        const startChar = str[start];
+        const startCount = countByChar.get(startChar);
+        if (startCount === 1) countByChar.delete(startChar);
+        else countByChar.set(startChar, startCount - 1);
+        start++;
+      }
+    }
+  }
+
+  return maxLength;
+}
+
+export function testLongestSubstr() {
+  log('Expected 7, got: ', longestSubstringWithKDistinct('aaaaabcde', 3));
+}
--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-import { testSmallestSubarray } from './algorithms_and_data_structures/patterns/sliding_window/smallest_subarray_with_given_sum';
+import { testLongestSubstr } from './algorithms_and_data_structures/patterns/sliding_window/longest_substring_with_k_distinct';
 
-testSmallestSubarray();
+testLongestSubstr();
