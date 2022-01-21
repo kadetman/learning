f7793e7f48b8cc2dd7c7b68556ede99267a12f53
kadetman <kadetman83@gmail.com>
2022-01-20 20:55:07 -0800

    fast and slow pointers - linked list cycle start node

--- algorithms_and_data_structures/patterns/fast_slow_pointers/linked_list_cycle.ts
+++ algorithms_and_data_structures/patterns/fast_slow_pointers/linked_list_cycle.ts
@@ -40,6 +40,25 @@ function calculateCycleLength(node: ILinkedListNode): number {
   return counter;
 }
 
+function getCycleStart(head: ILinkedListNode): ILinkedListNode | null {
+  let cycleLength = getCycleLength(head);
+  if (cycleLength === 0) return null;
+
+  let p1 = head,
+    p2 = head;
+  while (cycleLength) {
+    p2 = p2.next;
+    cycleLength--;
+  }
+
+  while (p1 !== p2) {
+    p2 = p2.next;
+    p1 = p1.next;
+  }
+
+  return p1;
+}
+
 export function testCycle() {
   const head: ILinkedListNode = { val: 0, next: null };
   head.next = { val: 1, next: null };
@@ -48,9 +67,11 @@ export function testCycle() {
   head.next.next.next.next = { val: 4, next: null };
 
   log('Expected false, got: ', hasCycle(head));
-  log('Expected 0, got: ', getCycleLength(head));
+  log('Expected 0 nodes, got: ', getCycleLength(head));
+  log('Expected null, got: ', getCycleStart(head)?.val ?? null);
 
   head.next.next.next.next = head;
   log('Expected true, got: ', hasCycle(head));
-  log('Expected 4, got: ', getCycleLength(head));
+  log('Expected 0 nodes, got: ', getCycleLength(head));
+  log('Expected node 0, got: ', getCycleStart(head)?.val ?? null);
 }
