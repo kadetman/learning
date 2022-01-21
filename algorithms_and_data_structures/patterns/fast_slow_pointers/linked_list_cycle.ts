342ca1a899ec1d65ef739d3d9b5fe2ee53e3ec4b
kadetman <kadetman83@gmail.com>
2022-01-20 20:40:47 -0800

    fast slow pointers - linked list cycle length

--- algorithms_and_data_structures/patterns/fast_slow_pointers/linked_list_cycle.ts
+++ algorithms_and_data_structures/patterns/fast_slow_pointers/linked_list_cycle.ts
@@ -14,6 +14,32 @@ function hasCycle(head: ILinkedListNode): boolean {
   return false;
 }
 
+function getCycleLength(head: ILinkedListNode): number {
+  let slow = head,
+    fast = head;
+  while (fast?.next) {
+    fast = fast.next?.next;
+    slow = slow.next;
+    if (fast === slow) {
+      return calculateCycleLength(slow);
+    }
+  }
+
+  return 0;
+}
+
+function calculateCycleLength(node: ILinkedListNode): number {
+  const slow = node;
+  let counter = 1;
+
+  while (node.next !== slow) {
+    counter++;
+    node = node.next;
+  }
+
+  return counter;
+}
+
 export function testCycle() {
   const head: ILinkedListNode = { val: 0, next: null };
   head.next = { val: 1, next: null };
@@ -22,7 +48,9 @@ export function testCycle() {
   head.next.next.next.next = { val: 4, next: null };
 
   log('Expected false, got: ', hasCycle(head));
+  log('Expected 0, got: ', getCycleLength(head));
 
   head.next.next.next.next = head;
   log('Expected true, got: ', hasCycle(head));
+  log('Expected 4, got: ', getCycleLength(head));
 }
