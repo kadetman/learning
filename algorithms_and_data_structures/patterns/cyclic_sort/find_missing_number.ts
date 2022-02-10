d773d11f4a7597cd7516830c7185d1c507e4b254
kadetman <kadetman83@gmail.com>
2022-02-10 09:32:12 -0800

    cyclic sort - find missing number - fix edge case

--- algorithms_and_data_structures/patterns/cyclic_sort/find_missing_number.ts
+++ algorithms_and_data_structures/patterns/cyclic_sort/find_missing_number.ts
@@ -13,12 +13,14 @@ function findMissingNumber(arr: number[]) {
     if (arr[i] !== i) return i;
   }
 
-  return -1;
+  // If all numbers are in the correct place - number "n" is missing.
+  return arr.length;
 }
 
 export function testMissingNumber() {
   log('Expected -1, got: ', findMissingNumber([]));
   log('Expected 1, got: ', findMissingNumber([0, 4, 3, 2]));
   log('Expected 2, got: ', findMissingNumber([4, 0, 3, 1]));
+  log('Expected 4, got: ', findMissingNumber([2, 0, 3, 1]));
   log('Expected 7, got: ', findMissingNumber([8, 3, 5, 2, 4, 6, 1, 0]));
 }
