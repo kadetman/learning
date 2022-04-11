af6bf7c964073f2982a132318f4570a3f535b2fc
kadetman <kadetman83@gmail.com>
2022-04-11 09:02:03 -0700

    modifiend binary search - rotations count - handle duplicates

--- algorithms_and_data_structures/patterns/modified_binary_search/rotations_count.ts
+++ algorithms_and_data_structures/patterns/modified_binary_search/rotations_count.ts
@@ -2,26 +2,28 @@ import { log } from '../../linked_list/utils';
 
 // https://www.educative.io/courses/grokking-the-coding-interview/R1v4P0R7VZw
 function getRotationsCount(arr: number[]): number {
-  if (!arr.length) return 0;
-
-  // Routations count is the same as an index of min number.
+  // Rotations count is the same as an index of min number.
   let start = 0,
     end = arr.length - 1;
   if (arr[start] < arr[end]) return start;
 
-  while (start <= end) {
+  while (start < end) {
     const mid = Math.floor(start + (end - start) / 2);
+    if (mid < end && arr[mid] > arr[mid + 1]) return mid + 1;
+    if (mid > start && arr[mid - 1] > arr[mid]) return mid;
+
+    if (arr[start] === arr[mid] && arr[mid] === arr[end]) {
+      if (arr[start] > arr[start + 1]) return start;
+      start++;
+      if (arr[end - 1] > arr[end]) return end;
+      end--;
+    }
     if (arr[start] < arr[mid]) {
       start = mid + 1;
-    } else if (arr[start] === arr[mid]) {
-      if (arr[mid] > arr[mid + 1]) return mid + 1;
-    } else {
-      if (arr[mid] < arr[mid - 1]) return mid;
-      else end = mid - 1;
-    }
+    } else end = mid - 1;
   }
 
-  return end;
+  return 0;
 }
 
 export function testRotationsCount() {
@@ -30,4 +32,5 @@ export function testRotationsCount() {
   log('Expected 4, got: ', getRotationsCount([1, 3, 8, 10, 0]));
   log('Expected 2, got: ', getRotationsCount([10, 15, 1, 3, 8]));
   log('Expected 5, got: ', getRotationsCount([4, 5, 7, 9, 10, -1, 2]));
+  log('Expected 3, got: ', getRotationsCount([3, 3, 7, 3]));
 }
