6620c5a748d793a8a9602754edf3ef77648ce916
kadetman <kadetman83@gmail.com>
2022-07-29 20:13:35 -0700

    quick sort fix

--- algorithms_and_data_structures/sorts/quick_sort.ts
+++ algorithms_and_data_structures/sorts/quick_sort.ts
@@ -5,27 +5,21 @@ function quickSort(arr: number[], start = 0, end = arr.length - 1): number[] {
 
   const pivot = partition(arr, start, end);
 
-  console.log('start', start, 'end', end, 'pivot', pivot);
   quickSort(arr, start, pivot - 1);
   quickSort(arr, pivot + 1, end);
 
   return arr;
 }
 
-function partition(arr: number[], start: number, end: number): number {
+function partition(arr, start, end) {
   const pivotIndex = end;
   const pivot = arr[pivotIndex];
-  end--;
 
   while (start < end) {
-    if (arr[start] > pivot && arr[end] < pivot) {
-      while (start < end && arr[start] < pivot) start++;
-      while (end > start && arr[end] > pivot) end--;
+    while (start < end && arr[start] < pivot) start++;
+    while (end > start && arr[end] >= pivot) end--;
 
-      [arr[start], arr[end]] = [arr[end], arr[start]];
-      start++;
-      end--;
-    }
+    [arr[start], arr[end]] = [arr[end], arr[start]];
   }
 
   [arr[pivotIndex], arr[start]] = [arr[start], arr[pivotIndex]];
@@ -35,5 +29,5 @@ function partition(arr: number[], start: number, end: number): number {
 
 export function testQuickSort() {
   log('Expected [0, 1, 2, 3, 4], got: ', quickSort([4, 3, 2, 1, 0]));
-  // log('Expected [0, 1, 2, 3, 4, 5], got: ', quickSort([5, 4, 3, 2, 1, 0]));
+  log('Expected [0, 1, 2, 3, 4, 5], got: ', quickSort([5, 4, 3, 2, 1, 0]));
 }
